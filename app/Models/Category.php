7999bdbba82a9157ddf76c418c98aeaf517bd89f
mmrtonmoybd <mmrtonmoybd@users.noreply.github.com>
2024-12-21 19:28:34 +0000

    Apply action changes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,38 +12,35 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Category
- * 
+ *
  * @property int $id
  * @property string $name
  * @property int|null $parent_id
  * @property Carbon|null $updated_at
  * @property Carbon|null $created_at
- * 
  * @property Category|null $category
  * @property Collection|Category[] $categories
- *
- * @package App\Models
  */
 class Category extends Model
 {
-	protected $table = 'categories';
+    protected $table = 'categories';
 
-	protected $casts = [
-		'parent_id' => 'int'
-	];
+    protected $casts = [
+        'parent_id' => 'int',
+    ];
 
-	protected $fillable = [
-		'name',
-		'parent_id'
-	];
+    protected $fillable = [
+        'name',
+        'parent_id',
+    ];
 
-	public function category()
-	{
-		return $this->belongsTo(Category::class, 'parent_id');
-	}
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
 
-	public function categories()
-	{
-		return $this->hasMany(Category::class, 'parent_id');
-	}
+    public function categories()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
